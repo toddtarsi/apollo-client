8c5fcb4daf2bcb3bc95c30aa531f471dbe3043b6
Todd tarsi <toddtarsi@Todds-MacBook-Pro.local>
2018-06-08 15:45:35 -0500

    hack: making things work for now

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -58,6 +58,7 @@ const defaultQueryInfo = {
   observableQuery: null,
   subscriptions: [],
 };
+const emptyList: any[] = [];
 
 export interface QueryPromise {
   resolve: (result: ApolloQueryResult<any>) => void;
@@ -473,8 +474,14 @@ export class QueryManager<TStore> {
 
       const errorStatusChanged =
         errorPolicy &&
-        (lastError && lastError.graphQLErrors) !==
-          queryStoreValue.graphQLErrors &&
+        (lastError && lastError.graphQLErrors && lastError.graphQLErrors.length ?
+          lastError.graphQLErrors :
+          emptyList
+        ) !== (
+          queryStoreValue && queryStoreValue.graphQLErrors && queryStoreValue.graphQLErrors.length ?
+            queryStoreValue.graphQLErrors :
+            emptyList
+        ) &&
         errorPolicy !== 'none';
 
       if (
